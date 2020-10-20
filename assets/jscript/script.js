d47c31142e5ca0375b96c580bea549b6518f401e
faisalnadeem78 <faisalnadeem_pk@hotmail.com>
2020-10-20 18:20:06 +0000

    Comments added in JS file and new Site logo added

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+    // Nav menu toggle button's click to close function
     $('.nav-button').click(function(){
         $('.nav-button').toggleClass('change');
     })
+    // Nav menu Active class
     $('.nav-link').click(function(){
         $('.nav-link').removeClass('active');
         $(this).addClass('active');
